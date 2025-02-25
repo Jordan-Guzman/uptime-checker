f0ff6d57629f9435c99968a3ac5163b085602e6c
Jordan-Guzman <jordanhguz@gmail.com>
2025-02-24 16:13:39 -0800

    first commit

---
+++ uptime_checker.py
@@ -0,0 +1,33 @@
+import requests
+import schedule
+import time
+
+# List of websites to monitor
+URLS = ["https://example.com", "https://yourclientside.com", "https://www.github.com", "https://downforeveryoneorjustme.com"]
+DISCORD_WEBHOOk = "https://discord.com/api/webhooks/1343700023287877703/yMt4UFl9vVGX6Cdacow_Vrdmr15NiB56U_-Y-xfTGbcIJZxqbwRZT36LvlbsiAxDPV5i"
+
+# Check website status
+def check_websites():
+	for url in URLS:
+		try:
+			response = requests.get(url, timeout=1)
+			if response.status_code == 200:
+				print(f"{url} is UP")
+			else:
+				print(f"{url} is DOWN")
+				send_alert(url)
+		except requests.exceptions.RequestException:
+			print(f"{url} is DOWN")
+			send_alert(url)
+
+# Schedule the check eveery minute
+schedule.every(1).minutes.do(check_websites)
+
+def send_alert(url):
+	message = {"content": f"{url} is DOWN!"}
+	requests.post(DISCORD_WEBHOOk, json=message)
+
+# Run the scheduler
+while True:
+	schedule.run_pending()
+	time.sleep(1)
